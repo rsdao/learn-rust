a1df0a7f61bae8509f199ff23f98d5fd4b138728
rsdao <rsdao@aliyun.com>
2023-10-23 22:48:51 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+pub mod course1;
+
+fn main() {
+    println!("[+] 'a'~'Z'之间的所有字符: ");
+    course1::print_lower_a_to_upper_z();
+    println!("\n[+] 'A'~'z'之间的所有字符: ");
+    course1::print_upper_a_to_lower_z::print_upper_a_to_lower_z();
+    println!();
+}
